6dfcd62755a12bfa824153511b4b2371abe990cd
Ron <ron@skullsecurity.net>
2012-08-08 15:09:57 -0500

    Initial import

---
+++ Match.h
@@ -0,0 +1,23 @@
+// Match.h
+// by iago
+// Sept. 10/2003
+//
+// These are basically functions for pattern-matching and replacing
+
+#ifndef MATCH_H
+#define MATCH_H
+
+#define tolower(a) (a >= 'A' && a <= 'Z' ? a + 0x20 : a)
+#define toupper(a) (a >= 'a' && a <= 'z' ? a - 0x20 : a)
+
+bool Replace(const char *Find, const char *Replace, char *String);
+bool ReplaceAll(const char *Find, const char *ReplaceChar, char *String);
+
+
+int Match(const char *mask, const char *name);
+inline char *RemoveSingleChar(char *String, int Location);
+inline char *InsertSingleChar(char *String, int Location, char Char);
+bool ReplaceAll(const char *Find, const char *ReplaceChar, char *String);
+bool Replace(const char *Find, const char *Replace, char *String);
+
+#endif
